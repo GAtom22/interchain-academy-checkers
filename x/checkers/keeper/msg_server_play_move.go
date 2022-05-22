11a66dc3af443960f920918f46b055a22a829737
Tom <tomasguerraalda@gmail.com>
2022-05-22 16:34:19 -0300

    add play move logic and tests

--- x/checkers/keeper/msg_server_play_move.go
+++ x/checkers/keeper/msg_server_play_move.go
@@ -2,16 +2,73 @@ package keeper
 
 import (
 	"context"
+	"strings"
 
+	"github.com/alice/checkers/x/checkers/rules"
 	"github.com/alice/checkers/x/checkers/types"
 	sdk "github.com/cosmos/cosmos-sdk/types"
+	sdkerrors "github.com/cosmos/cosmos-sdk/types/errors"
 )
 
 func (k msgServer) PlayMove(goCtx context.Context, msg *types.MsgPlayMove) (*types.MsgPlayMoveResponse, error) {
 	ctx := sdk.UnwrapSDKContext(goCtx)
 
-	// TODO: Handling the message
-	_ = ctx
+	// 1. Get the stored game
+	storedGame, found := k.Keeper.GetStoredGame(ctx, msg.IdValue)
+	if !found {
+		return nil, sdkerrors.Wrapf(types.ErrGameNotFound, "game not found %s", msg.IdValue)
+	}
 
-	return &types.MsgPlayMoveResponse{}, nil
+	// 2. Check if is legitimate player
+	isRed := strings.Compare(storedGame.Red, msg.Creator) == 0
+	isBlack := strings.Compare(storedGame.Black, msg.Creator) == 0
+	var player rules.Player
+	if !isRed && !isBlack {
+		return nil, types.ErrCreatorNotPlayer
+	} else if isRed && isBlack {
+		player = rules.StringPieces[storedGame.Turn].Player
+	} else if isRed {
+		player = rules.RED_PLAYER
+	} else {
+		player = rules.BLACK_PLAYER
+	}
+
+	// 3. Instantiate board to implement the rules
+	game, err := storedGame.ParseGame()
+	if err != nil {
+		panic(err.Error())
+	}
+
+	// 4. Check if is the player's turn to play
+	if !game.TurnIs(player) {
+		return nil, types.ErrNotPlayerTurn
+	}
+
+	// 5. Conduct the move
+	captured, moveErr := game.Move(
+		rules.Pos{
+			X: int(msg.FromX),
+			Y: int(msg.FromY),
+		},
+		rules.Pos{
+			X: int(msg.ToX),
+			Y: int(msg.ToY),
+		},
+	)
+	if moveErr != nil {
+		return nil, sdkerrors.Wrapf(types.ErrWrongMove, moveErr.Error())
+	}
+
+	// 6. Prepare the updated board to be stored
+	storedGame.Game = game.String()
+	storedGame.Turn = rules.PieceStrings[game.Turn]
+	k.Keeper.SetStoredGame(ctx, storedGame)
+
+	// Return relevant information
+	return &types.MsgPlayMoveResponse{
+		IdValue:   msg.IdValue,
+		CapturedX: int64(captured.X),
+		CapturedY: int64(captured.Y),
+		Winner:    game.Winner().Color,
+	}, nil
 }
