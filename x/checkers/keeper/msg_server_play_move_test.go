3541497d7d83730e674bf177202f57e1c47424f5
Tom <tomasguerraalda@gmail.com>
2022-05-22 21:59:34 -0300

    add test for reject-game

--- x/checkers/keeper/msg_server_play_move_test.go
+++ x/checkers/keeper/msg_server_play_move_test.go
@@ -86,12 +86,13 @@ func TestPlayMoveSavedGame(t *testing.T) {
 	game1, found := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
-		Creator: alice,
-		Index:   "1",
-		Game:    "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:    "r",
-		Red:     bob,
-		Black:   carol,
+		Creator:   alice,
+		Index:     "1",
+		Game:      "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|********|r*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "r",
+		Red:       bob,
+		Black:     carol,
+		MoveCount: 1,
 	}, game1)
 }
 
@@ -176,12 +177,13 @@ func TestPlayMove2SavedGame(t *testing.T) {
 	game1, found := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
-		Creator: alice,
-		Index:   "1",
-		Game:    "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|*r******|**r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:    "b",
-		Red:     bob,
-		Black:   carol,
+		Creator:   alice,
+		Index:     "1",
+		Game:      "*b*b*b*b|b*b*b*b*|***b*b*b|**b*****|*r******|**r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "b",
+		Red:       bob,
+		Black:     carol,
+		MoveCount: 2,
 	}, game1)
 }
 
@@ -254,12 +256,13 @@ func TestPlayMove3SavedGame(t *testing.T) {
 	game1, found := keeper.GetStoredGame(sdk.UnwrapSDKContext(context), "1")
 	require.True(t, found)
 	require.EqualValues(t, types.StoredGame{
-		Creator: alice,
-		Index:   "1",
-		Game:    "*b*b*b*b|b*b*b*b*|***b*b*b|********|********|b*r*r*r*|*r*r*r*r|r*r*r*r*",
-		Turn:    "r",
-		Red:     bob,
-		Black:   carol,
+		Creator:   alice,
+		Index:     "1",
+		Game:      "*b*b*b*b|b*b*b*b*|***b*b*b|********|********|b*r*r*r*|*r*r*r*r|r*r*r*r*",
+		Turn:      "r",
+		Red:       bob,
+		Black:     carol,
+		MoveCount: 3,
 	}, game1)
 }
 
