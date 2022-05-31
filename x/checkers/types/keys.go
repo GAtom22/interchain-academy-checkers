a4bbc31a0bb362cca4e528e93744fab18e741a8b
Tom <tomasguerraalda@gmail.com>
2022-05-31 18:11:52 -0300

    add endblock remove expired games

--- x/checkers/types/keys.go
+++ x/checkers/types/keys.go
@@ -51,10 +51,16 @@ const (
 )
 
 const (
-    NoFifoIdKey = "-1"
+	NoFifoIdKey = "-1"
 )
 
 const (
-    MaxTurnDuration = time.Duration(24 * 3_600 * 1000_000_000) // 1 day
-    DeadlineLayout  = "2006-01-02 15:04:05.999999999 +0000 UTC"
+	MaxTurnDuration = time.Duration(24 * 3_600 * 1000_000_000) // 1 day
+	DeadlineLayout  = "2006-01-02 15:04:05.999999999 +0000 UTC"
+)
+
+const (
+	ForfeitGameEventKey     = "GameForfeited"
+	ForfeitGameEventIdValue = "IdValue"
+	ForfeitGameEventWinner  = "Winner"
 )
